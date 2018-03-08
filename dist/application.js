6785bab2ce716b4970048b6cfaf049e5fc18b761
Aleksandra Kurek <kurek.aleksandra@gmail.com>
2018-03-08 08:23:37 +0100

    #header: added mobile menu

--- dist/application.js
+++ dist/application.js
@@ -3,4 +3,10 @@ $('a').click(function () {
     $('html,body').unbind().animate({scrollTop: $($.attr(this, 'href')).offset().top}, 'slow');
 });
 
+/* Burger menu */
 
+$('#burger').click(function () {
+    console.log('hello');
+    $('#burger-content').toggleClass('hide-md');
+    $('#burger-content').toggleClass('open-burger');
+});
