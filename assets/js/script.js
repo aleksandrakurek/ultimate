b35bd1a5a7c85a0591c0428b57d0ae9ac22bf068
Aleksandra Kurek <kurek.aleksandra@gmail.com>
2018-03-09 10:28:09 +0100

    #draw: change images on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,40 @@ $('a').click(function () {
 /* Burger menu */
 
 $('#burger').click(function () {
-    console.log('hello');
     $('#burger-content').toggleClass('hide-md');
     $('#burger-content').toggleClass('open-burger');
+});
+
+/* Smooth scrolling */
+$('a').click(function () {
+    $('html,body').unbind().animate({scrollTop: $($.attr(this, 'href')).offset().top}, 'slow');
+});
+
+/* Burger menu */
+
+$('#burger').click(function () {
+    $('#burger-content').toggleClass('hide-md');
+    $('#burger-content').toggleClass('open-burger');
+});
+
+
+/* Draws */
+
+const groundFloorLeft = $('#grFlrL');
+const groundFloorRight = $('#grFlrR');
+const floorLeft = $('#flrL');
+const floorRight = $('#flrR');
+const imgDraw = $('#sketch');
+
+groundFloorLeft.click(function () {
+    imgDraw.attr("src", "assets/images/draw.png");
+});
+groundFloorRight.click(function () {
+    imgDraw.attr("src", "assets/images/draw1.png");
+});
+floorLeft.click(function () {
+    imgDraw.attr("src", "assets/images/draw2.png");
+});
+floorRight.click(function () {
+    imgDraw.attr("src", "assets/images/draw3.png");
 });
